e388177da00a086db8cd87258ab326577b47e088
arthur jinian <aj@ajinian.com>
2020-12-06 15:17:54 -0800

    Added custom pizza add button.

--- NennosPizza/Screens/Home/HomeController.swift
+++ NennosPizza/Screens/Home/HomeController.swift
@@ -14,6 +14,7 @@ class HomeController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     let viewModel = HomeViewModel()
+    @IBOutlet weak var customPizzaButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
