416dd48e404c4c278f23dc5bdc85252895e7d55c
arthur jinian <aj@ajinian.com>
2020-12-07 14:54:09 -0800

    Fixing breaking constraints

--- NennosPizza/CustomViews/Extensions.swift
+++ NennosPizza/CustomViews/Extensions.swift
@@ -10,7 +10,6 @@ import UIKit
 extension UIViewController {
     func setLeftAlignedNavigationItemTitle(text: String) {
         let titleLabel = UILabel ()
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.textColor = UIColor(displayP3Red: 1, green: 0.31, blue: 0.27, alpha: 1.0)
         titleLabel.font = UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
         titleLabel.numberOfLines = 0
@@ -20,13 +19,9 @@ extension UIViewController {
         navigationItem.titleView = titleLabel
         guard let containerView = navigationController?.navigationBar else { return }
         let leftBarItemWidth = navigationItem.leftBarButtonItems?.reduce(0, { $0 + $1.width })
-        let rightBarItemWidth = navigationItem.rightBarButtonItems?.reduce(0, { $0 + $1.width })
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
             titleLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor,
-                                             constant: leftBarItemWidth ?? 32),
-            titleLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: rightBarItemWidth ?? 32)
+                                             constant: leftBarItemWidth ?? 32)
         ])
     }
 }
